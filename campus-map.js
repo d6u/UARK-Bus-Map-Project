38940345f86d67f6e04d2c60616831276d5fdf56
Daiwei Lu <daiweilu123@gmail.com>
2012-10-31 16:12:48 -0500

    Added reduced service display

--- campus-map.js
+++ campus-map.js
@@ -1,6 +1,16 @@
 (function (w) {
+	var touch = (mobileBrowser) ? 'touchend' : 'click', // mapping touchend and click event
+		// MODERNIZR TRANSITION END EVENT
+		transEndEventNames = {
+		    'WebkitTransition' : 'webkitTransitionEnd',
+		    'MozTransition'    : 'transitionend',
+		    'OTransition'      : 'oTransitionEnd',
+		    'msTransition'     : 'MSTransitionEnd',
+		    'transition'       : 'transitionend'
+		},
+		transEnd = transEndEventNames[ Modernizr.prefixed('transition') ],
 		// GOOGLE MAP OPTIONS
-	var mapOptions = {
+		mapOptions = {
 			center: new google.maps.LatLng(36.068000, -94.172500),
 			zoom: 14,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -10,7 +20,103 @@
 		// INIT GOOGLE MAP
 		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions),
 		// INIT ROUTES STORAGE
-		routes = {};
+		routes = {},
+		buses = [];
+	// DECLEAR FUNCTIONS
+	function translate (color) { // TRANSLATE COLOR INTO ROUTE ID
+		var code = {n: null, rd: null};
+		switch (color) {
+			case 'red':
+				code.n = 1;
+				code.rd = 1;
+				break;
+			case 'brown':
+				code.n = 12;
+				break;
+			case 'tan':
+				code.n = 15;
+				code.rd = 15;
+				break;
+			case 'yellow':
+				code.n = 16;
+				break;
+			case 'mapleHill':
+				code.n = 18;
+				break;
+			case 'purple':
+				code.n = 19;
+				code.rd = 24;
+				break;
+			case 'pomfret':
+				code.n = 20;
+				break;
+			case 'route56':
+				code.n = 21;
+				break;
+			case 'blue':
+				code.n = 8;
+				code.rd = 22;
+				break;
+			case 'green':
+				code.n = 71;
+				code.rd = 23;
+				break;
+			case 'gray':
+				code.n = 88;
+				break;
+		}
+		return code;
+	} // TRANSLATE COLOR INTO ID
+	function showRoute(id) { // DISPLAY ROUTE PATH & BUS POSITION
+		var bus_id = drawPath(id);
+		if (bus_id != null) {
+			pinBus(bus_id);
+			setInterval(function () {
+				pinBus(bus_id);
+			}, 4000);
+		}
+	}
+	function drawPath(id) { // DRAW ROUTE PATH
+		if (routes[id.n].inService == 1 || routes[id.rd].inService == 1) {
+			var current_id = routes[id.n].inService == 1 ? id.n : id.rd;
+			var shape = routes[current_id].shape,
+				points = shape.split(','),
+				routes_path = [];
+			for (var i = 0; i < points.length; i++) {
+				var pair = {};
+				pair.a = Number(points[i].split(' ')[0]);
+				pair.b = Number(points[i].split(' ')[1]);
+				var path = new google.maps.LatLng(pair.a, pair.b);
+				routes_path[i] = path;
+			}
+			var routes_polyline = new google.maps.Polyline({
+				path: routes_path,
+				strokeColor: routes[current_id].color,
+				strokeOpacity: 0.8,
+				strokeWeight: 4
+			});
+			routes_polyline.setMap(map);
+			return current_id;
+		} else {
+			alert('No Service is available now.');
+			return null;
+		}
+	}
+	function pinBus(bus_id) {
+		var url = 'http://campusdata.uark.edu/api/buses?callback=?&routeIds=' + bus_id;
+		$.getJSON(url, function (data) {
+			for (var i = 0; i < data.length; i++) {
+				var lat = data[i].latitude,
+					lng = data[i].longitude;
+				if (buses[i]) buses[i].setMap(null);
+				var new_bus = new google.maps.Marker({
+						position: new google.maps.LatLng(lat, lng),
+						map: map
+					});
+				buses[i] = new_bus;
+			}
+		});
+	}
 	// LOAD ROUTES INFO
 	$.ajax({
 		url: 'http://campusdata.uark.edu/api/routes',
@@ -27,50 +133,15 @@
 			});
 		},
 		error: function () {
-			console.log('ERROR');
+			alert('ERROR');
 		}
-	})
-	.done(function () {
-		console.log(routes);
-		// SHOW ROUTE PATH
-		var shape = routes[19].shape,
-			points = shape.split(','),
-			routes_path = [];
-		for (var i = 0; i < points.length; i++) {
-			var pair = {a: null, b: null};
-			pair.a = Number(points[i].split(' ')[0]);
-			pair.b = Number(points[i].split(' ')[1]);
-			var path = new google.maps.LatLng(pair.a, pair.b);
-			routes_path[i] = path;
-		}
-		var routes_polyline = new google.maps.Polyline({
-			path: routes_path,
-			strokeColor: routes[19].color,
-			strokeOpacity: 0.8,
-			strokeWeight: 4
-		});
-		routes_polyline.setMap(map);
 	});
-	// SHOW BUS POSITION
-	var bus;
-	function showBus(id) {
-		var url = 'http://campusdata.uark.edu/api/buses?callback=?&routeIds=' + id;
-		$.getJSON(url, function (data) {
-			console.log(data);
-			console.log(data[0].latitude, data[0].longitude);
-			if (bus) bus.setMap(null);
-			var new_bus = new google.maps.Marker({
-					position: new google.maps.LatLng(data[0].latitude, data[0].longitude),
-					map: map,
-					title: 'purple bus'
-				});
-			bus = new_bus;
-		});
-	}
-	// SET TIME INTERVAL TO UPDATE BUS POSITION
-	showBus(19);
-	setInterval(function () {
-		showBus(19);
-	}, 10000);
+	// ATTACH EVENT TO MENU BUTTON
+	$('#menu').on(touch, 'a', function (e) {
+		var rt_color = $(this).attr('class').split(' ')[1],
+			rt_code = translate(rt_color);
+		$('#menu-overlay').css({display: 'none'});
+		showRoute(rt_code);
+	});
 })(window);
 
