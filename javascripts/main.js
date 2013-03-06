5e4bbb018cff4b83e2489c630d17de4b7fae155d
Daiwei Lu <daiweilu123@gmail.com>
2013-03-05 21:33:11 -0600

    Add bus schedule table

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		"brown": 15,
 		"tan": [15, 15],
 		"yellow": 16,
-		"mapleHill": 18,
+		"maple-hill": 18,
 		"purple": [19, 24],
 		"pomfret": 20,
 		"route56": 21,
@@ -35,7 +35,7 @@ $(document).ready(function() {
 	var route_polyline = null; // route path
 	var route_stops = []; // stop markers
 	var bus_positions = []; // position markers
-	var updateBusPositionTimer;
+	var updateBusPositionTimer = null; // update bus position timer
 	
 	// detect user location
 	// Try W3C Geolocation
@@ -43,7 +43,9 @@ $(document).ready(function() {
 		navigator.geolocation.getCurrentPosition(
 			// success
 			function(position) {
-				map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+				// change map center to current user location
+				map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+				// mark current user location
 				userPosition = new google.maps.Marker({
 					position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
 					icon:{
@@ -54,11 +56,11 @@ $(document).ready(function() {
 					map: map
 				});
 				watchPosition = navigator.geolocation.watchPosition(
-					// success
+					// success: update marker position after successfully update GPS data
 					function(position) {
 						userPosition.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
 					},
-					// fail
+					// fail: clear watcher after fail
 					function() {
 						navigator.geolocation.clearWatch(watchPosition);
 					}
@@ -69,16 +71,17 @@ $(document).ready(function() {
 	
 	// Bookmark functions
 	if (location.hash != "")
-	// has bookmark
 	{
 		var hashString = location.hash.replace('#', '');
 		// hashtag is valid
-		if (routesID[hashString] != undefined) {
+		if (routesID[hashString] != undefined)
+		{
 			loadRoute( routesID[hashString] );
 			$('.top-menu').css({display: 'none'});
 		}
 		// hashtag is invalid
-		else {
+		else
+		{
 			location.hash = '';
 		};
 	};
@@ -99,15 +102,23 @@ $(document).ready(function() {
 		$.getJSON('http://campusdata.uark.edu/api/routes?callback=?', 
 			{routeid: normalID}, 
 			function(response) {
-				if (response.inService) {
+				if ( response.inService || normalID == reducedID )
+				{
 					showRoute(response);
 					showStops(normalID);
-					showBusPositions(normalID);
-					updateBusPositionTimer = setInterval(function(){
+					
+					if ( response.inService )
+					{
 						showBusPositions(normalID);
-					},4000);
+						updateBusPositionTimer = setInterval(function(){
+							showStops(normalID);
+							showBusPositions(normalID);
+						},4000);
+					};
 				}
-				else {
+				// normal service is not available and routes has reduced service
+				else
+				{
 					// load reduced path
 					$.getJSON('http://campusdata.uark.edu/api/routes?callback=?',
 						{routeid: reducedID},
@@ -115,15 +126,18 @@ $(document).ready(function() {
 							showRoute(response);
 							showStops(reducedID);
 							
-							if (response.inService) {
+							if (response.inService)
+							{
 								showBusPositions(reducedID);
 								updateBusPositionTimer = setInterval(function(){
+									showStops(reducedID);
 									showBusPositions(reducedID);
 								},4000);
 							};
 						});
 				};
-		});
+			} // end of getJSON callback
+		); // end of getJSON
 	}
 	
 	function showRoute(route) { // DISPLAY ROUTE PATH & BUS POSITION
@@ -148,45 +162,85 @@ $(document).ready(function() {
 	
 	function showStops(routeID) {
 		
-		$.getJSON('http://campusdata.uark.edu/api/stops?callback=?', {routeids: routeID}, function (response) {
-			for (var i = 0; i < response.length; i++)
-			{
-				var lat = response[i].latitude, lng = response[i].longitude;
-				var new_stop = new google.maps.Marker({
-					position: new google.maps.LatLng(lat, lng),
-					icon:{
-						url: 'images/stop_icon.svg',
-						scaledSize: new google.maps.Size(21, 21),
-						anchor: new google.maps.Point(10, 10)
-					},
-					map: map
-				});
-				route_stops[i] = new_stop;
-			}
-		});
+		$.getJSON('http://campusdata.uark.edu/api/stops?callback=?',
+			{routeids: routeID},
+			function (response) {
+				
+				// clean stops
+				if (route_stops.length > 0) {
+					for (var i=0; i < route_stops.length; i++) {
+						route_stops[i].setMap(null);
+					};
+				};
+				
+				// Hold stops DOM node
+				var stopInfoDOMArray = [];
+				
+				for (var i = 0; i < response.length; i++)
+				{
+					// hold stop object
+					var stop = response[i];
+					
+					// pin stop marker
+					var lat = stop.latitude, lng = stop.longitude;
+					var new_stop = new google.maps.Marker({
+						position: new google.maps.LatLng(lat, lng),
+						icon:{
+							url: 'images/stop_icon.svg',
+							scaledSize: new google.maps.Size(21, 21),
+							anchor: new google.maps.Point(10, 10)
+						},
+						map: map
+					});
+					
+					// save stop marker
+					route_stops[i] = new_stop;
+					
+					// fill schedule table
+					var a = $('<a/>').attr({href: '#'}).append(
+						$('<div/>').addClass('stop-name').html(stop.name),
+						$('<div/>').addClass('arriving-time').html(stop.nextArrival)
+					);
+					stopInfoDOMArray.push(a);
+				} // end of for loop
+				$('.stop-container').empty().append(stopInfoDOMArray);
+			} // end of getJSON callback
+		); // end of getJSON
 	}
 	
+	// showBusPositions() also in charge of changing schedule clock
 	function showBusPositions(routeID) {
 		
-		$.getJSON('http://campusdata.uark.edu/api/buses?callback=?', {routeids: routeID}, function (response) {
-			if (bus_positions.length > 0) {
-				for (var i=0; i < bus_positions.length; i++) {
-					bus_positions[i].setMap(null);
+		$.getJSON('http://campusdata.uark.edu/api/buses?callback=?', 
+			{routeids: routeID},
+			function (response)
+			{
+				if (bus_positions.length > 0)
+				{
+					// clear bus_position markers
+					for (var i=0; i < bus_positions.length; i++)
+					{
+						bus_positions[i].setMap(null);
+					};
 				};
-			};
-			for (var i = 0; i < response.length; i++) {
-				var lat = response[i].latitude, lng = response[i].longitude;
-				var new_bus = new google.maps.Marker({
-					position: new google.maps.LatLng(lat, lng),
-					map: map,
-					// icon:{
-// 						url: 'bus_icon.svg',
-// 						scaledSize: new google.maps.Size (27.5, 27.5)
-// 					}
-				});
-				bus_positions[i] = new_bus;
-			}
-		});
+				
+				// hold value for shortest
+				var shortestMin = null;
+				for (var i = 0; i < response.length; i++)
+				{
+					var lat = response[i].latitude, lng = response[i].longitude;
+					var new_bus = new google.maps.Marker({
+						position: new google.maps.LatLng(lat, lng),
+						map: map,
+						// icon:{
+						// 	url: 'bus_icon.svg',
+						// 	scaledSize: new google.maps.Size (27.5, 27.5)
+						// }
+					});
+					bus_positions[i] = new_bus;
+				}
+			} // end of getJSON callback
+		); // end of getJSON
 	}
 	
 	// clean map function
@@ -244,5 +298,18 @@ $(document).ready(function() {
 		}, 1);
 	});
 	
+	// Show stops arriving schedule
+	$('a[href="#route-schedule"]').on(touch, function(event) {
+		
+		event.preventDefault();
+		$('.route-schedule').css({display: "block"});
+	});
+	
+	// Close stops arriving schedule
+	$('a[href="#close-route-schedule"]').on(touch, function(event) {
+		
+		event.preventDefault();
+		$('.route-schedule').css({display: "none"});
+	})
 	
 }); // end of document ready
